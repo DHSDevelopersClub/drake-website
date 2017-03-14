6fefc0113c5c0a7d450648215819fc7f476e289f
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-03-14 15:50:55 -0700

    Code clean up.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,3 @@
-// Removed for dev
-
 // if ('serviceWorker' in navigator) {
 //   navigator.serviceWorker.register('./sw.js').then(reg => {
 //     console.log('[SW] Registered');
@@ -8,32 +6,29 @@
 //   });
 // };
 
-// fetch('../views/home.html', { method: 'GET' })
-// .then(resp => {
-//   resp.blob().then(data => {
-//     console.log(data);
-//   });
-// })
-// .catch(err => {
-//   console.warn('Fetch error: ', err);
-// });
-
 const teacherCache = localStorage;
 
 const classList = document.getElementById('class-list');
+const pinned = document.getElementById('pinned');
 const search = document.getElementById('search');
 
 let wordSearch = '';
 
-console.log(search);
+function writeToList(i, data) {
+  let classDiv = document.createElement('div');
+  teacherCache.setItem(i, 'star-empty.svg');
+  let first = `<img src="/res/${teacherCache.getItem(i)}" alt="Star" class="star">`;
+  classDiv.className = 'class';
+  classDiv.setAttribute('id', i);
+  classDiv.innerHTML = `${first}<div class="class-name">${data[i].class}</div><div class="teacher-name">${data[i].name}</div>`;
+  classList.appendChild(classDiv);
+}
 
 function updateList(word) {
-  fetch('teacher-pages.json', _ => {
-
-  })
+  fetch('teacher-pages.json')
   .then(function(resp){
-    let data = resp.json(() => {
-    }).then(data => {
+    let data = resp.json()
+    .then(data => {
       if (word) {
         classList.innerHTML = '';
         for (let i = 0; i < data.length; i++) {
@@ -41,36 +36,24 @@ function updateList(word) {
           let className = data[i].class.toLowerCase();
           let teacherName = data[i].name.toLowerCase();
           if (className.includes(word) || teacherName.includes(word)) {
-            let classDiv = document.createElement('div');
-            teacherCache.setItem(i, 'star-empty.svg');
-            let first = `<img src="/res/${teacherCache.getItem(i)}" alt="Star" class="star">`;
-            classDiv.className = 'class';
-            classDiv.setAttribute('id', i);
-            classDiv.innerHTML = `${first}<div class="class-name">${data[i].class}</div><div class="teacher-name">${data[i].name}</div>`;
-            classList.appendChild(classDiv);
+            writeToList(i, data);
           }
         }
       } else {
         classList.innerHTML = '';
         for (let i = 0; i < data.length; i++) {
-          let classDiv = document.createElement('div');
-          teacherCache.setItem(i, 'star-empty.svg');
-          let first = `<img src="/res/${teacherCache.getItem(i)}" alt="Star" class="star">`;
-          classDiv.className = 'class';
-          classDiv.setAttribute('id', i);
-          classDiv.innerHTML = `${first}<div class="class-name">${data[i].class}</div><div class="teacher-name">${data[i].name}</div>`;
-          classList.appendChild(classDiv);
+          writeToList(i, data);
         }
       }
-    })
-  })
+    });
+  });
 }
 
 search.addEventListener('keyup', evt => {
   let keycode = evt.keyCode;
   let valid = 
         (keycode > 47 && keycode < 58)   || // number keys
-        keycode == 32   || // spacebar & return key(s) (if you want to allow carriage returns)
+        (keycode == 32)                  || // spacebar & return key(s) (if you want to allow carriage returns)
         (keycode > 64 && keycode < 91)   || // letter keys
         (keycode > 95 && keycode < 112)  || // numpad keys
         (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
@@ -82,7 +65,6 @@ search.addEventListener('keyup', evt => {
     } else {
       wordSearch += evt.key;
     }
-    console.log(wordSearch);
     updateList(wordSearch);
   }
 });

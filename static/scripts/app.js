eac32b3cd3c3faf2d544d34baea97260edbd9ce0
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-03-14 23:22:44 -0700

    Broken pinning.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -6,47 +6,97 @@
 //   });
 // };
 
+// STRUCTURE
+
+/*
+declare vars
+Get data
+write to screen all cards
+add listeners
+*/
+
+// VARS
 const teacherCache = localStorage;
-
-const classList = document.getElementById('class-list');
 const pinned = document.getElementById('pinned');
+const classList = document.getElementById('class-list');
 const search = document.getElementById('search');
-
 let wordSearch = '';
+
+// GET DATA
+let outerData;
+
+fetch('teacher-pages.json')
+.then(resp => {
+  data = resp.json()
+  .then(data => {
+    outerData = data;
+    updateList(data);
+    writePinned(data);
+    addEventListeners();
+  })
+});
 
 function writeToList(i, data) {
   let classRow = document.createElement('tr');
-  teacherCache.setItem(i, 'star-empty.svg');
-  let first = `<span><img src="/res/${teacherCache.getItem(i)}" alt="Star" class="star"></span>`;
+  let first = `<span><img src="/res/star-empty.svg" alt="Star" class="star" id="${i}"></span>`;
   classRow.className = 'class';
-  classRow.setAttribute('id', i);
+  // classRow.setAttribute('id', i);
   classRow.innerHTML = `${first}<td class="class-name">${data[i].class}</td><td class="teacher-name">${data[i].name}</td>`;
   classList.appendChild(classRow);
 }
 
-function updateList(word) {
-  fetch('teacher-pages.json')
-  .then(function(resp){
-    let data = resp.json()
-    .then(data => {
-      if (word) {
-        classList.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
-          word = word.toLowerCase();
-          let className = data[i].class.toLowerCase();
-          let teacherName = data[i].name.toLowerCase();
-          if (className.includes(word) || teacherName.includes(word)) {
-            writeToList(i, data);
-          }
-        }
-      } else {
-        classList.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
-          writeToList(i, data);
-        }
+// Write ALL data to screen
+function updateList(data, word) {
+  if (word) {
+    classList.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+      word = word.toLowerCase();
+      let className = data[i].class.toLowerCase();
+      let teacherName = data[i].name.toLowerCase();
+      if (className.includes(word) || teacherName.includes(word)) {
+        writeToList(i, data);
       }
+    }
+  } else {
+    classList.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+      writeToList(i, data);
+    }
+  }
+}
+
+function writePinned(data) {
+  if (teacherCache.length != 0) {
+    for (let i = 0; i < teacherCache.length; i++) {
+      let classRow = document.createElement('tr');
+      let first = `<span><img src="/res/star-gold.svg" alt="Star" class="star" id="${i}"></span>`;
+      classRow.className = 'class';
+      classRow.innerHTML = `${first}<td class="class-name">${data[i].class}</td><td class="teacher-name">${data[i].name}</td>`;
+      pinned.appendChild(classRow);
+    }
+  }
+}
+
+function updatePinned(id, data) {
+  let classRow = document.createElement('tr');
+  let first = `<span><img src="/res/star-gold.svg" alt="Star" class="star" id="${id}"></span>`;
+  classRow.className = 'class';
+  classRow.innerHTML = `${first}<td class="class-name">${data[id].class}</td><td class="teacher-name">${data[id].name}</td>`;
+  pinned.appendChild(classRow);
+}
+
+function addEventListeners() {
+  let stars = document.getElementsByClassName('star');
+
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener('click', evt => {
+      let id = evt.path[0].id;
+      evt.path[0].classList.add('pinned');
+      console.log(evt.path[0].classList);
+      teacherCache.setItem(id, id);
+      updatePinned(id, outerData);
     });
-  });
+  }
 }
 
 search.addEventListener('keyup', evt => {
@@ -65,8 +115,6 @@ search.addEventListener('keyup', evt => {
     } else {
       wordSearch += evt.key;
     }
-    updateList(wordSearch);
+    updateList(outerData, wordSearch);
   }
 });
-
-updateList();

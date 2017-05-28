203a89902912a9c87fad21b124222accbc7f6f2d
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2017-05-28 16:22:28 -0700

    Add template, converter

--- amp_cov.py
+++ amp_cov.py
@@ -4,9 +4,9 @@ import htmlmin
 with open('test.html', 'r') as f:
     html_doc = f.read()
     html_doc = htmlmin.minify(unicode(html_doc, "utf-8"), remove_empty_space=True)
+    f.close()
 
 soup = BeautifulSoup(html_doc, 'html.parser')
-
 content = soup.find(id="module-content-9604")
 
 for element in content(text=lambda text: isinstance(text, Comment)):
@@ -15,4 +15,16 @@ for element in content(text=lambda text: isinstance(text, Comment)):
 for elem in content.findAll(['script', 'style', 'xml']):
     elem.extract()
 
-print(content)
+with open('template.html', 'r') as g:
+    template = g.read()
+    template = htmlmin.minify(unicode(template, "utf-8"), remove_empty_space=True)
+    g.close()
+    template = BeautifulSoup(template, 'html.parser')
+
+template.body.insert(0, content)
+
+print(template)
+
+html = template.prettify("utf-8")
+with open("output.html", "wb") as file:
+    file.write(html)
